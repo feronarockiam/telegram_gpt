0f2b355b5c8ef5e182fe12816ecaff95c2b5e47a
Anupriya-rgb <feronarockiam1493@gmail.com>
2023-05-28 12:38:08 +0530

    key

--- tel.py
+++ tel.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, executor, types
 bot = Bot(token='5954457249:AAGPuWzfnEBY92lPWbjZUsLkHlmxeg0_7QY')
 dp = Dispatcher(bot)
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 @dp.message_handler(commands = ["start","help"])
 async def welcome(message: types.Message):
