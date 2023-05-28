2dd04fd12e67bdabcfb897a72e02f40ce21e1350
Anupriya-rgb <feronarockiam1493@gmail.com>
2023-05-28 10:43:18 +0530

    initial

---
+++ tel.py
@@ -0,0 +1,29 @@
+import os
+import openai
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token='5954457249:AAGPuWzfnEBY92lPWbjZUsLkHlmxeg0_7QY')
+dp = Dispatcher(bot)
+
+openai.api_key = "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"
+
+@dp.message_handler(commands = ["start","help"])
+async def welcome(message: types.Message):
+    await message.reply("Hi! I am Scrappy.Ai! Ask me something...")
+
+@dp.message_handler()
+async def gpt(message:types.Message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        max_tokens=1024,
+        temperature=0.7,
+        top_p=0.9,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+    await message.reply(response.choices[0].text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
